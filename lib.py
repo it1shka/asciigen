8f59bc424f5ee62da5bc5fe5b840478e595e24a9
it1shka <2tishbel@gmail.com>
2023-08-06 23:47:38 +0200

    Now it's kinda working

---
+++ lib.py
@@ -0,0 +1,39 @@
+from PIL import Image
+import numpy as np
+from math import floor
+
+
+ASCII_TAPE = ' .,:;ox%#@'
+DEFAULT_WIDTH = 50
+DEFAULT_HEIGHT = 50
+
+
+def image_to_matrix(image: Image) -> np.array:
+    image = image.convert('L')
+    array = np.array(image)
+    return array / 256.0
+
+
+def mean_to_ascii(mean: float) -> str:
+    index = floor(mean * len(ASCII_TAPE))
+    symbol = ASCII_TAPE[index]
+    return symbol
+
+
+def transform(image: Image, out_width: int = DEFAULT_WIDTH, out_height: int = DEFAULT_HEIGHT) -> str:
+    width, height = image.size
+    frame_width = width // out_width
+    frame_height = height // out_height
+    matrix = image_to_matrix(image)
+    lines = []
+    for row in range(0, height, frame_height):
+        line = ''
+        for col in range(0, width, frame_width):
+            extracted_frame = matrix[row:row + frame_height, col:col + frame_width]
+            mean_value = extracted_frame.mean()
+            ascii_value = mean_to_ascii(mean_value)
+            line += ascii_value
+        lines.append(line)
+    output = '\n'.join(lines)
+    return output
+
--- main.py
+++ main.py
@@ -1,56 +1,97 @@
 import sys
 from textwrap import dedent
 from PIL import Image
-from transformer import Transformer
+from lib import transform
+from os import fsync
 
 
-FRAME_WIDTH = 3
-FRAME_HEIGHT = 3
-ASCII_TAPE = ' .,:;ox%#@'
-
-
-def application() -> None:
+def start_console_application() -> None:
     print(dedent('''
         You entered application mode of asciigen.
         
         Now you will be asked to prompt image paths 
         to convert those images to ascii art.
         
+        The input has a format:
+        <image-path> width:<output-width> height:<output-height> file:<output-file>
+        
         To exit the application, type "exit".
     '''))
     index = 1
-    transformer = Transformer(ASCII_TAPE, FRAME_WIDTH, FRAME_HEIGHT)
     while True:
-        user_input = input(f'({index})> ')
-        if user_input == 'exit':
+        raw_input = input(f'({index})> ')
+        index += 1
+        input_tokens = raw_input.strip().split()
+        if not input_tokens:
+            print('No input provided. ')
+            continue
+        start_token, rest_tokens = input_tokens[0], input_tokens[1:]
+        if start_token == 'exit':
+            print('Exiting the application. ')
             return
         try:
-            image = Image.open(user_input)
-            ascii = transformer.apply(image)
-            print(ascii)
+            image = Image.open(start_token)
         except FileNotFoundError:
-            print('File does not exist')
+            print(f'Image "{start_token}" does not exist. ')
+            continue
+        params = tokens_to_dict(rest_tokens)
+        size = get_size_from_params(params)
+        ascii_output = transform(image, *size)
+        print(ascii_output)
+        if 'file' not in params:
+            continue
+        output_file_path = params['file']
+        with open(output_file_path, 'w+') as output_file:
+            output_file.write(ascii_output)
 
 
-def telegram_bot() -> None:
+def get_size_from_params(params: dict[str, str]) -> map:
+    output = []
+    if 'width' in params:
+        output.append(params['width'])
+    if 'height' in params:
+        output.append(params['height'])
+    return map(int, output)
+
+
+def tokens_to_dict(tokens: list[str]) -> dict[str, str]:
+    output = dict()
+    for token in tokens:
+        parts = token.split(':')
+        if len(parts) != 2:
+            print(f'Wrong format of argument: {token}. ')
+            continue
+        key, value = parts
+        output[key] = value
+    return output
+
+
+def start_telegram_bot() -> None:
     print(dedent('''
         You entered telegram bot mode of asciigen.
         
-        Hit Ctrl+C or Ctrl+D.
+        Hit Ctrl+C or Ctrl+D to stop it.
     '''))
     print('TODO: This is incomplete')
     sys.exit(1)
+    # TODO: ...
+
+
+def get_mode() -> str:
+    if len(sys.argv) < 2:
+        output = input('Please, provide a mode (app/bot): ')
+        output = output.strip()
+    else:
+        output = sys.argv[1]
+    return output
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        mode = input('Please, provide a mode (app/bot): ')
-    else:
-        mode = sys.argv[1]
+    mode = get_mode()
     if mode == 'app':
-        application()
+        start_console_application()
     elif mode == 'bot':
-        telegram_bot()
+        start_telegram_bot()
     else:
         print(f'Unknown mode: {mode}')
         sys.exit(1)
